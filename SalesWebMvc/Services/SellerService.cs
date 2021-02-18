60bfd1887d158351cbc9dde98e0561a88058a1b7
Lucas Inocencio Pires <piresilucas@gmail.com>
2021-02-17 21:19:33 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -26,5 +26,17 @@ namespace SalesWebMvc.Services
             _context.Seller.Add(seller);
             _context.SaveChanges();
         }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.SingleOrDefault(sg => sg.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
     }
 }
